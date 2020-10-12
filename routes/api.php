3c3e9d51976799cb6e8e96688111d83a03dc68d5
Ibn Sums <i.sljon143@gmail.com>
2020-10-13 01:50:24 +0600

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::apiResource('/products','App\Http\Controllers\ProductController@ProductController');
+Route::group(['prefix'=>'products'], function(){
+    Route::apiResource('/{product}/reviews','App\Http\Controllers\ReviewController@ReviewController');
 });
